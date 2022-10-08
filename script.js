2fec7377ad2546914ad82ea4600c3ecfcbdefbc6
Bryan <bryanmonterom@gmail.com>
2022-10-08 14:16:11 -0400

    add screenshot

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-let populateGrid = (value,color)=>{
-    const gridContainer = document.querySelector('.grid-container');
-    for (let index = 0; index < value; index++) {
-        let gridRow = document.createElement('div');
-        gridRow.classList.add('grid-row');
-        gridContainer.appendChild(gridRow);
-
-       for (let j = 0; j < value; j++) {
-        let grid = document.createElement('div');
-        grid.addEventListener('mouseover',()=>{ grid.style.backgroundColor = color/*classList.add('hover')*/})
-        grid.classList.add('grid');
-        gridRow.appendChild(grid);
-        ;
-       }
-    }
-}
-
-
-function updateGrid(){
-cleanGrid();
-drawGrid();
-}
-
 let slider = document.getElementById('sizeSlider');
 let color = document.getElementById('favcolor');
 
-slider.addEventListener('change',updateSliderValue);
 
-function updateSliderValue(){
+let populateGrid = (value, color) => {
+    const gridContainer = document.querySelector('.grid-container');
+
+    for (let i = 0; i < value; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        gridContainer.appendChild(gridRow);
+        for (let j = 0; j < value; j++) {
+            let grid = document.createElement('div');
+            grid.addEventListener('mouseover', () => {
+                grid.style.backgroundColor = color
+            });
+            grid.classList.add('grid');
+            gridRow.appendChild(grid);
+        }
+    }
+}
+
+slider.addEventListener('change', updateSliderValue);
+
+function updateSliderValue() {
     let sliderValue = document.querySelector('#sliderValue');
     sliderValue.textContent = `${slider.value} x ${slider.value}`;
 }
 
-function drawGrid(){
+
+function updateGrid() {
+    cleanGrid();
+    drawGrid();
+}
+
+function drawGrid() {
     updateSliderValue();
     populateGrid(slider.value, color.value);
 }
 
-function cleanGrid(){
+function cleanGrid() {
     const grids = document.querySelectorAll('.grid-row');
-    console.log(grids);
-    grids.forEach(a=> a.remove());
+    grids.forEach(a => a.remove());
+
 }
 
-
 drawGrid();
+
