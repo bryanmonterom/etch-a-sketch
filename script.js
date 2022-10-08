99f62ad10f93ff732b422549e9a1007dbe877b46
Bryan <bryanmonterom@gmail.com>
2022-10-07 22:24:15 -0400

    Add controls to dinamically handle the grid

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let populateGrid = ()=>{
+let populateGrid = (value)=>{
     const gridContainer = document.querySelector('.grid-container');
-    for (let index = 0; index < 9; index++) {
+    for (let index = 0; index < value; index++) {
         let gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridContainer.appendChild(gridRow);
 
-       for (let j = 0; j < 9; j++) {
+       for (let j = 0; j < value; j++) {
         let grid = document.createElement('div');
+        grid.addEventListener('mouseover',()=>{ grid.classList.add('hover')})
         grid.classList.add('grid');
         gridRow.appendChild(grid)
         ;
@@ -14,4 +15,30 @@ let populateGrid = ()=>{
     }
 }
 
-populateGrid();
+
+function updateGrid(){
+cleanGrid();
+drawGrid();
+}
+
+let slider = document.getElementById('sizeSlider');
+slider.addEventListener('change',updateGrid);
+
+function updateSliderValue(){
+    let sliderValue = document.querySelector('#sliderValue');
+    sliderValue.textContent = `${slider.value} x ${slider.value}`;
+}
+
+function drawGrid(){
+    updateSliderValue();
+    populateGrid(slider.value);
+}
+
+function cleanGrid(){
+    const grids = document.querySelectorAll('.grid-row');
+    console.log(grids);
+    grids.forEach(a=> a.remove());
+}
+
+
+drawGrid();
